de024530f4db1d7a463f95ddd6c3cbbdb1e69077
viswa199 <lingareddyviswanadhreddy@gmail.com>
2022-12-12 12:51:32 +0530

    first commit

---
+++ hello-world/app.js
@@ -0,0 +1,99 @@
+const axios = require('axios')
+require('dotenv').config();
+// const url = 'http://checkip.amazonaws.com/';
+let response;
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+exports.lambdaHandler = async (event, context) => {
+    const body = JSON.parse(event.body)
+
+    const customer= body.customer;
+    const {id,email,first_name,last_name,phone}=customer
+    const customer_name=first_name+last_name
+
+    const {lineitems} = body 
+    const {name} = lineitems[0]
+
+    const token = process.env.API_KEY
+    try {
+            //checking if the person is in the pipedrive, if not we will add the person in the pipedrive.
+        const hasPerson = await getPerson(name)
+        if(!hasPerson){
+            const customer_params = {
+                name:customer_name,
+                email:email,
+                phone:phone,
+                owner_id:id
+            }
+            const ret=await axios.post(`https://api.pipedrive.com/v1/persons?api_token=${token}`,customer_params)
+        }
+     
+            //checking, if the product is in the pipedrive, if not we will add product to pipedrive.
+        const hasProduct = await getProduct(name)
+        if(!hasProduct){
+            const product_params ={
+                name:name
+            }
+        const ret = await axios.post(`https://api.pipedrive.com/v1/products?api_token=${token}`,product_params)
+       }
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'hello world',
+                // location: ret.data.trim()
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
+
+const getPerson = async(url,name)=>{
+    const firstChar=name[0];
+    var data=await axios.get(`https://api.pipedrive.com/v1/persons?first_char=${firstChar}&api_token=${token}`)
+    data = data.data.data
+
+    if(data==null){
+        return 0
+    }
+
+    for(let item of data){
+        if(item.name==name){
+            return 1
+        }
+    }
+    return 0
+}
+
+const getProduct = async(name)=>{
+    const firstChar=name[0]
+    var data = await axios.get(`https://api.pipedrive.com/v1/products?first_char=${firstChar}&api_token=${token}`)
+    data = data.data.data;
+
+    if(data==null){
+        return 0
+    }
+    
+    for(let item of data){
+        console.log(item.name)
+        if(item.name==name){
+            return 1
+        }
+    }
+
+    return 0
+}
